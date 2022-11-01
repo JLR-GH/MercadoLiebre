3c61b303f6145b9dfed135dbd32648ff9bd2ab57
JLR-GH <ripapropiedades@gmail.com>
2022-11-01 20:18:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+let PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('Servidor corriendo en el puerto ' + PORT);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
